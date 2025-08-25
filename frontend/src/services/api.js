d6822c7cf0727584d81b1ac73792ddc3a7f0b3dd
Devnyk <dn8128292@gmail.com>
2025-08-25 18:08:13 +0530

    update3

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/api", // your backend url
+  baseURL: "https://files-uploader-zm1n.onrender.com/api", // your backend url
 });
 
 export default api;
